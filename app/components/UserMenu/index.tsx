8a086e2d8732c166b1db43b1940e2f821ce2ae45
Tirivashe <shaymusts@gmail.com>
2023-08-31 14:53:33 +0200

    menu on header

--- app/components/UserMenu/index.tsx
+++ app/components/UserMenu/index.tsx
@@ -1,17 +1,27 @@
-import { Avatar, Button, Group } from "@mantine/core";
+import { Avatar, Button, Group, Menu, rem } from "@mantine/core";
 import { IconMenu2 } from "@tabler/icons-react";
 import styles from "./user-menu.module.scss";
+import { useDisclosure } from "@mantine/hooks";
 
 const UserMenu = () => {
+  const [opened, { toggle }] = useDisclosure();
   return (
     <Group gap="md" className={styles.root}>
       <Button visibleFrom="md" size="compact-md">
         Airbnb Your Home
       </Button>
-      <Group gap="xs" justify="space-around">
-        <IconMenu2 />
-        <Avatar radius="xl" />
-      </Group>
+      <Menu opened={opened} onChange={toggle}>
+        <Menu.Target>
+          <Group gap="xs" justify="space-around">
+            <IconMenu2 />
+            <Avatar radius="xl" />
+          </Group>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item>Login</Menu.Item>
+          <Menu.Item>Sign Up</Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
     </Group>
   );
 };
